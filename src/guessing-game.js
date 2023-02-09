54ff1f4bb103435efb78a0b2b045930ca2209ec1
Anton Leshkovich <antonleshkovich@mail.ru>
2023-02-09 17:34:12 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
+    
     constructor() {}
-
+    
     setRange(min, max) {
-
+         this.minNum = min;
+         this.maxNum = max;
     }
-
+    
     guess() {
-
+        return this.guessNum = Math.round((this.maxNum + this.minNum) / 2);
     }
-
+    
     lower() {
-
+        return this.maxNum = this.guessNum;
     }
-
+    
     greater() {
-
+        return this.minNum = this.guessNum;
     }
 }
 
